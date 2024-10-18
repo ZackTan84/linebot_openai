530984bca48743f309857349ccf88ed1789da300
ZackTan84 <134357095+ZackTan84@users.noreply.github.com>
2024-10-18 17:47:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ conversation_history = []
 def GPT_response(messages):
     # 接收回應
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4o-mini-2024-07-18",
         messages=messages,
         temperature=0.5,
         max_tokens=500
