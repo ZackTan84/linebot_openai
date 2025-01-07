885b175ad1c829b4eb51cadc3897a5600bffad66
ZackTan84 <134357095+ZackTan84@users.noreply.github.com>
2025-01-07 10:04:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def load_assistant_data():
 def GPT_response(messages):
     # 接收回應
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0125",
+        model="gpt-4o-mini-2024-07-18",
         messages=messages,
         temperature=0.5,
         max_tokens=500
